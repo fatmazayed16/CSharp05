da3ccd45f9ecabb4dc5364adbab8d78135375546
فاطمه محمود السيد عبدالرحيم زايد <fatma30982329@f-eng.tanta.edu.eg>
2026-09-14 04:21:55 +0300

    Q2

--- C#Basic05/Program.cs
+++ C#Basic05/Program.cs
@@ -8,6 +8,11 @@
             double[] prices = { 25.5, 40.0, 33.75 };
             Console.WriteLine($"the second price is {prices[1]}");
             #endregion
+
+            #region Q2
+            int[,] shelfCopies = { { 3,5 },{1,4 } };
+            Console.WriteLine(shelfCopies[1,0]);
+            #endregion
         }
     }
 }
