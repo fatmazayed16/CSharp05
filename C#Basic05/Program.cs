c02040d3b2454e35a0c173637eeeba1a5bb0d4fa
فاطمه محمود السيد عبدالرحيم زايد <fatma30982329@f-eng.tanta.edu.eg>
2026-09-14 04:14:35 +0300

    Q1

--- C#Basic05/Program.cs
+++ C#Basic05/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine($"the second price is {prices[1]}");
+            #endregion
         }
     }
 }
